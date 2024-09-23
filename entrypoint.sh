ba8838e4fd831e650d320992765b662eeb58ed91
Emerson Connelly <emerconn7@gmail.com>
2024-09-22 23:02:11 -0500

    better logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-echo "[TASK] cd to: ${ARCHIVE_FILE_DIRECTORY}"
+echo "[TASK] directory: ${ARCHIVE_FILE_DIRECTORY}"
 cd ${ARCHIVE_FILE_DIRECTORY}
 
-echo "[TASK] setting ARCHIVE_FILE_NAME to most recent filename: $(ls -t | head -1)"
+echo "[TASK] using most recent file: $(ls -t | head -1)"
 ARCHIVE_FILE_NAME=$(ls -t | head -1)
 
-echo "[TASK] moving ${ARCHIVE_FILE_NAME} to ${SMB_SERVICE_NAME} at ${SMB_DIRECTORY}"
+echo "[TASK] moving /"${ARCHIVE_FILE_NAME}/" to /"${SMB_SERVICE_NAME}/${SMB_DIRECTORY}/""
 smbclient -U=${SMB_USER_NAME}@${SMB_USER_DOMAIN}%${SMB_USER_PASSWORD} ${SMB_SERVICE_NAME} --directory ${SMB_DIRECTORY} -c "put ${ARCHIVE_FILE_NAME}"
