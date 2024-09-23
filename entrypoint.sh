38809136884a891dc90c2f2e9452f4190a8f940b
Emerson Connelly <emerconn7@gmail.com>
2024-09-22 23:03:28 -0500

    use correct escape character

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ cd ${ARCHIVE_FILE_DIRECTORY}
 echo "[TASK] using most recent file: $(ls -t | head -1)"
 ARCHIVE_FILE_NAME=$(ls -t | head -1)
 
-echo "[TASK] moving /"${ARCHIVE_FILE_NAME}/" to /"${SMB_SERVICE_NAME}/${SMB_DIRECTORY}/""
+echo "[TASK] moving \"${ARCHIVE_FILE_NAME}\" to \"${SMB_SERVICE_NAME}/${SMB_DIRECTORY}\""
 smbclient -U=${SMB_USER_NAME}@${SMB_USER_DOMAIN}%${SMB_USER_PASSWORD} ${SMB_SERVICE_NAME} --directory ${SMB_DIRECTORY} -c "put ${ARCHIVE_FILE_NAME}"
